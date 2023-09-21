6d9d308f1d48a9c2aff4f8b5f77d5345318e928e
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-09-21 11:30:55 +0200

    Update roland.m

--- 1. félév/matviz/roland.m
+++ 1. félév/matviz/roland.m
@@ -1,7 +1,8 @@
-% dot() a*b, belső szorzat
-% norm() hossza
-% rad2deg() radian fokba
-% cross(a,b) axb, vektoriális szorzat
-% plot(a,b) egy ábrát mutat ki
+% dot() -> a*b, belső szorzat
+% norm() -> hossza
+% rad2deg() -> radian fokba
+% cross(a,b) -> axb, vektoriális szorzat
+% plot(a,b) -> egy ábrát mutat ki
 % figure -> külön ablakba teszi az utána levő ábrát
+% axis([[-1.5 2.5 0.5 3.5]) -> koordináta rendszert képez
 % <-- commmandok vege -->

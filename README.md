0173897560b9e803e9a990bfe9e090742542b0af
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-09-21 11:07:16 +0200

    com

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# matviz
+# Itt találod a Debreceni Egyetem Informatikai Kar óráit, lebontva félévekre
+# PTI képzés
--- roland.m
+++
@@ -1,5 +0,0 @@
-% dot() a*b, belso szorzat
-% norm() hossza
-% rad2deg() radian fokba
-% cross(e,d) axb, vektorialis szorzat
-% <-- commmandok vege -->

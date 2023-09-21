2b3b71d639bcef2ffbcb6ce0071818580ea28daa
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-09-21 11:40:10 +0200

    Update roland.m

--- 1. félév/matviz/roland.m
+++ 1. félév/matviz/roland.m
@@ -5,4 +5,6 @@
 % plot(a,b) -> egy ábrát mutat ki
 % figure -> külön ablakba teszi az utána levő ábrát
 % axis([[-1.5 2.5 0.5 3.5]) -> koordináta rendszert képez
+% linspace(a,b,n) -> a-tól b-ig, n számot generál(számtani sorozat féle)
+% a:b:c -> a-tól c-ig b-enként lép
 % <-- commmandok vege -->

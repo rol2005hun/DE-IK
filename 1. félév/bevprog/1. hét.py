dbe7c1c94d226bd1b585438d9bfcee87eae4bbce
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-09-21 14:10:54 +0200

    Update 1. hét.py

--- 1. félév/bevprog/1. hét.py
+++ 1. félév/bevprog/1. hét.py
@@ -1,21 +1,21 @@
 #1. hf
 import math
 
-sugar = float(input("Add meg a kor sugarat: "))
+sugar = float(input("Add meg a kör sugarát: "))
 
-kerulet = 2 * sugar * (math.pi)
-terulet = sugar ** 2 * (math.pi)
+kerulet = 2 * sugar * math.pi
+terulet = sugar ** 2 * math.pi
 
-print("Kerulet: ", kerulet)
-print("Terulet", terulet)
+print("Kerület: ", kerulet)
+print("Terület: ", terulet)
 
 #2. hf
 
 import math
 
-szam1 = float(input("Add meg az elso szamot: "))
+szam1 = float(input("Add meg az első számot: "))
 
-szam2 = float(input("Add meg a masodik szamot: "))
+szam2 = float(input("Add meg a második számot: "))
 
-print("Szamtani kozep: ", (szam1 + szam2) / 2)
-print("Mertani kozep: ", math.sqrt(szam1 * szam2))
+print("Számtani közép: ", (szam1 + szam2) / 2)
+print("Mértani közép: ", math.sqrt(szam1 * szam2))

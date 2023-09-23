48271def1ec5a5e2a328157d9e8ec12783947d93
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-09-23 12:44:45 +0300

    Create 3. hét hf.py

---
+++ 1. félév/bevprog/3. hét hf.py
@@ -0,0 +1,11 @@
+#1. hf
+
+n = int(input("Hány műveletet szeretnél elvégeztetni? "))
+
+for i in range(n):
+    expression = input()
+    try:
+        result = eval(expression)
+        print("Eredménye: ", result)
+    except ValueError:
+        print("Hiba történt!")

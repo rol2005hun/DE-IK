e79f04eec08f2b4dabe7c7916443af40bd60b423
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-09-23 12:50:36 +0300

    Update 3. hét hf.py

--- 1. félév/bevprog/3. hét hf.py
+++ 1. félév/bevprog/3. hét hf.py
@@ -9,3 +9,14 @@ for i in range(n):
         print("Eredménye: ", result)
     except ValueError:
         print("Hiba történt!")
+
+#1. hf
+
+n = int(input("Add meg hány számot olvasol be: "))
+ma = input("")
+
+for i in range(n - 1):
+    if input("") > ma:
+        ma = input("")
+
+print("A maximum: ", ma)

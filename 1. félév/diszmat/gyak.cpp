174d43923a7522e644c80ddd2ced2094130fecfd
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-09-26 17:30:25 +0300

    Update gyak.cpp

--- 1. félév/diszmat/gyak.cpp
+++ 1. félév/diszmat/gyak.cpp
@@ -1,14 +1,27 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
+int lnko(int a, int b) { // ez az euklédiszi algoritmus, amely a legnagyobb közös osztót keresi
+    if(b == 0) {
+        return a;
+    } else {
+        return lnko(b, a % b);
+    }
+}
+
+int x, k= 0;
+
 int main() {
     int c = 0;
     for(int i = 1; i<= 88200; i++){
         if(88200%i==0) {
             c++;
+            x = lnko(i, 110);
+            if(x == 1) k++;
         }
     }
     
-    cout << c;
+    cout << c << ' ' << k;
 
     return 0;
 }

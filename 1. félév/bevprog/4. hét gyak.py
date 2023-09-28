097a4013a200dd3668eb1913f92c2a6a17ad7f24
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-09-28 14:45:06 +0200

    Update 4. hét gyak.py

--- 1. félév/bevprog/4. hét gyak.py
+++ 1. félév/bevprog/4. hét gyak.py
@@ -5,10 +5,26 @@ n = 1
 
 while n != 0:
     n = int(input(""))
-    if(n > 0):
+    if n > 0:
         pozitiv += 1
-    elif(n < 0):
+    elif n < 0:
         negativ += 1
 
 print("Pozitív számok száma: ", pozitiv)
 print("Negatív számok száma: ", negativ)
+
+#2. feladat
+paros = 0
+paratlan = 0
+n = 0
+
+while n < 100:
+    n = int(input(""))
+    if n % 2 == 0:
+        paros += 1
+    elif n % 2 != 0:
+        paratlan += 1
+    n += n
+
+print("Páros számok száma: ", paros)
+print("Páratlan számok száma: ", paratlan)

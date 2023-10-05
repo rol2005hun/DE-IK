949277e331891f6b6dc86b09d9da5e3fdb60bc23
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-10-05 11:32:45 +0200

    Update roland.m

--- 1. félév/matviz/roland.m
+++ 1. félév/matviz/roland.m
@@ -3,6 +3,7 @@
 % rad2deg() -> radin fokba
 % cross(a,b) -> axb, vektoriális szorzat
 % plot(a,b) -> explicit alak, egy ábrát mutat ki
+% ezplot(f) -> függvény kirajzoltatása
 % figure -> külön ablakba teszi az utána levő ábrát
 % axis([-1.5 2.5 0.5 3.5]) -> koordináta rendszert képez
 % linspace(a,b,n) -> a-tól b-ig, n számot generál(számtani sorozat féle)
@@ -10,4 +11,5 @@
 % hold on/off -> tartsa meg az ablakokat, ne törölje/törölje
 % legend('1', '2') -> felülre tesz egy magyarázó boxot
 % fimplicit(@(x,y) x) -> implicit alakja a fv-eknek
+% solve(f) -> megoldja az f(x)=0 egyenletet
 % <-- commmandok vége -->

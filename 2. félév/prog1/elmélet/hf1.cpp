10f0b88c9aca2cf53ef0ba3ab844b1dc671b20db
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-02-22 18:51:17 +0100

    new

--- 2. félév/prog1/elmélet/hf1.cpp
+++ 2. félév/prog1/elmélet/hf1.cpp
@@ -1,1 +1,13 @@
-aa
+#include <iostream>
+using namespace std;
+
+int fullProcessor(int a, int b) {
+    int c = a * b;
+    int d = c * c;
+    return fullProcessor(c, d);
+}
+
+int main() {
+    fullProcessor(2, 3);
+    return 0;
+}

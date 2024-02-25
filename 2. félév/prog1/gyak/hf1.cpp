dd212d5082e7e586ed77e7d2968da7a4f2ea9018
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-02-25 15:15:45 +0200

    Create hf1.cpp

---
+++ 2. félév/prog1/gyak/hf1.cpp
@@ -0,0 +1,104 @@
+#include <iostream> 
+using namespace std; 
+  
+int main() {  
+  cout << "hello world!"
+  return 0;  
+}
+
+// ----------------------------------------------------
+
+#include <iostream>
+using namespace std;
+ 
+void main()
+{
+    int x = 10;
+    int y = 15; 
+     
+    cout << " "<< (x, y)
+}
+
+// ---------------------------
+
+#include <iostream>
+using namespace std;
+ 
+int main(void)
+{
+    while(.) 
+    {
+        cout <<"hello";
+    }
+    return 0;
+}
+
+// -----------------------------------------
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void main()
+{
+    int n = 9, div = 0;
+    div = n/0;
+     
+   cout << "result = "<< div;
+}
+
+// --------------------------
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void Main()
+{
+    int a = 10;
+    cout << " "<< a; 
+}
+
+//-------------------
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a, b, c;
+    a + b = c;
+}
+
+//-----------
+#include <iostream>
+using namespace std;
+int main() {
+  cout << x;
+}
+
+// --------------------------------
+#include <stdio.h>
+ 
+int main()
+{
+    char* str;
+ 
+    str = "GfG";
+ 
+    *(str + 1) = 'n';
+    return 0;
+}
+
+// ---------------
+
+#include <iostream>
+using namespace std;
+int main() {
+  int x;
+  x = 3.25;
+}
+
+// --------------------------
+#include <iostream>
+using namespace std;
+int main() {
+  int v[3];
+  v[4] = 3;
+}

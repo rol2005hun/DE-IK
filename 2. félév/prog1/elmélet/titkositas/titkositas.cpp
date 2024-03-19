94b6f3fc46115fc42b7be9cb9e0319a11aacee6a
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-03-19 23:19:30 +0200

    Update titkositas.cpp

--- 2. félév/prog1/elmélet/titkositas/titkositas.cpp
+++ 2. félév/prog1/elmélet/titkositas/titkositas.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-fstream file("alma.txt", ios::in | ios::out | ios::binary);
-
 string xorEncrypt(const string& text, const string& key) {
     string result = text;
     for (size_t i = 0; i < text.size(); i++) {
@@ -16,9 +14,19 @@ string xorEncrypt(const string& text, const string& key) {
 
 int main() {
     string fajlszoveg;
+    string fajlnev;
     string nemtitkositott;
     string kulcs;
     string titkositott;
+
+    cout << "Kerlek add meg a titkositando fajl nevet: ";
+    cin >> fajlnev;
+
+    fstream file(fajlnev, ios::in | ios::out | ios::binary);
+    if(!file) {
+        cout << "Nem letezik ilyen fajl!" << endl;
+        return 1;
+    }
 
     cout << "Kerlek add meg a kulcsot (ne felejtsd el, kell a visszafejteshez): ";
     cin >> kulcs;
@@ -39,7 +47,7 @@ int main() {
     file << titkositott;
     file.close();
 
-    cout << "Alap szoveg: " << nemtitkositott << endl;
+    cout << "Alap szoveg: \n" << nemtitkositott << endl;
     cout << "Titkositott szoveg: " << titkositott << endl;
 
     return 0;

bf4694bba39f8c710e946e53d825f61cc907c77c
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-04-07 14:52:27 +0300

    Update zh1.sql

--- 2. félév/adatb/zh1.sql
+++ 2. félév/adatb/zh1.sql
@@ -1,10 +1,10 @@
 select megrendeles, kontener, round(rakomanysuly, 2) from hajo.s_hozzarendel where rakomanysuly > 15
 select * from hajo.s_kikoto where leiras like '%kikötőméret: kicsi%' and leiras like '%mobil daruk%'
-select * from hajo.s_ut where to_char(indulasi_ido, 'MI') != '00'
-select hajo_tipus, count(hajo_tipus) from hajo.s_hajo where max_sulyterheles > 500 group by hajo_tipus
-select to_char(megrendeles_datuma, 'YYYY, MM') from hajo.s_megrendeles group by to_char(megrendeles_datuma, 'YYYY, MM') having count(to_char(megrendeles_datuma, 'YYYY, MM')) > 6 order by to_char(megrendeles_datuma, 'YYYY, MM')
-select vezeteknev || ' ' || keresztnev, telefon from hajo.s_ugyfel where helyseg in (select helyseg_id from hajo.s_helyseg where orszag = 'Szíria')
-select s_hajo_tipus.nev, min(s_hajo.netto_suly) from hajo.s_hajo_tipus join hajo.s_hajo on hajo_tipus = s_hajo.hajo_tipus group by s_hajo_tipus.nev
-select orszag || ' - ' || helysegnev from hajo.s_helyseg join hajo.s_kikoto on helyseg = helyseg_id where orszag in (select orszag from hajo.s_orszag where foldresz = 'Ázsia') order by helysegnev
-select nev, hajo_id, indulasi_kikoto, erkezesi_kikoto from hajo.s_hajo join hajo.s_ut on hajo_id = hajo where indulasi_ido = (select max(indulasi_ido) from hajo.s_ut)
+select to_char(indulasi_ido, 'YYYY.MM.DD HH:MI:SS') from hajo.s_ut where to_char(indulasi_ido, 'MI') != '00' order by to_char(indulasi_ido, 'YYYY.MM.DD HH:MI')
+select hajo_tipus, count(*) from hajo.s_hajo where max_sulyterheles > 500 group by hajo_tipus
+select to_char(megrendeles_datuma, 'YYYY.MM'), count(*) from hajo.s_megrendeles group by to_char(megrendeles_datuma, 'YYYY.MM') having count(*) >=6 order by to_char(megrendeles_datuma, 'YYYY.MM')
+select vezeteknev, keresztnev, telefon from hajo.s_ugyfel where helyseg in (select helyseg_id from hajo.s_helyseg where orszag = 'Szíria')
+select s_hajo_tipus.nev, min(netto_suly) from hajo.s_hajo_tipus join hajo.s_hajo on hajo_tipus_id = hajo_tipus group by s_hajo_tipus.nev
+select s_orszag.orszag, helysegnev from hajo.s_orszag join hajo.s_helyseg on s_helyseg.orszag = s_orszag.orszag where s_orszag.foldresz = 'Ázsia' and helyseg_id in (select helyseg from hajo.s_kikoto) order by helysegnev
+select nev, hajo_id, indulasi_kikoto, erkezesi_kikoto from hajo.s_hajo join hajo.s_ut on hajo = hajo_id where indulasi_ido = (select max(indulasi_ido) from hajo.s_ut)
 select erkezesi_kikoto from hajo.s_ut where indulasi_ido = (select min(indulasi_ido) from hajo.s_ut)

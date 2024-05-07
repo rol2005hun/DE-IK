3293f04437c8b2374f981fdc158ef668ef711c39
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-05-07 15:34:34 +0200

    Update zh2-2022-05-10(2).sql

--- 2. félév/adatb/zh2-2022-05-10(2).sql
+++ 2. félév/adatb/zh2-2022-05-10(2).sql
@@ -13,7 +13,39 @@ where helyseg_id in (
 order by orszag, helysegnev
 
 --3. feladat
-select  from hajo.s_hajo join hajo.s_ut on hajo = hajo_id
+select indulasi_kikoto, erkezesi_kikoto from hajo.s_hajo join hajo.s_ut on hajo = hajo_id
 where nev = 'SC Nina'
+order by erkezesi_ido - indulasi_ido
 fetch first 3 rows only
-order by 
+
+--4. feladat
+create table s_szemelyzet (
+    azonosito varchar(10) primary key,
+    vezeteknev varchar(50),
+    keresztnev varchar(50),
+    szuletesi_datum date,
+    telefonszam number(20) not null,
+    tartozik varchar(10) references hajo.s_hajo
+)
+
+--5. feladat
+create table s_kikoto_telefon as select * from hajo.s_kikoto_telefon; --ez csak egy temp utasítás, alsó kell a ZH-ra
+alter table s_kikoto_telefon drop constraint ide a nev
+
+--6. feladat
+insert into s_hajo select hajo_id, s_hajo.nev, netto_suly, max_kontener_dbszam, max_sulyterheles, hajo_tipus
+from hajo.s_hajo join hajo.s_hajo_tipus on hajo_tipus = hajo_tipus_id where s_hajo_tipus.nev = 'Small feeder' and netto_suly > 250
+
+--7. feladat
+create table s_ugyfel as select * from hajo.s_ugyfel; --ez csak egy temp utasítás, alsó kell a ZH-ra
+update s_ugyfel set keresztnev = upper(keresztnev), vezeteknev = upper(vezeteknev)
+where (vezeteknev, keresztnev) in (select vezeteknev, keresztnev from ( 
+    select ugyfel_id, keresztnev, vezeteknev, sum(fizetett_osszeg) as fo from hajo.s_ugyfel join hajo.s_megrendeles on ugyfel = ugyfel_id
+    group by ugyfel_id, keresztnev, vezeteknev
+) where fo in (select max(fo) from (
+    select ugyfel_id, keresztnev, vezeteknev, sum(fizetett_osszeg) as fo from hajo.s_ugyfel join hajo.s_megrendeles on ugyfel = ugyfel_id
+    group by ugyfel_id, keresztnev, vezeteknev
+)))
+
+--8. feladat
+majd maskor

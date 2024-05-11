cc693509d86e4809a9e2c5fae7b2ee8936bb6080
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-05-11 15:55:18 +0300

    Update zh2-2022-05-10(4).sql

--- 2. félév/adatb/zh2-2022-05-10(4).sql
+++ 2. félév/adatb/zh2-2022-05-10(4).sql
@@ -27,3 +27,46 @@ create table s_kikoto_email (
 )
 
 --5. feladat
+alter table s_kikoto_telefon drop constraint ideanev
+
+--6. feladat
+insert into s_hajo
+select h.* from hajo.s_hajo h
+join hajo.s_hajo_tipus on hajo_tipus = hajo_tipus_id
+where max_kontener_dbszam <= 10
+
+--7. feladat
+drop table s_helyseg;
+drop table s_orszag;
+create table s_helyseg as select * from hajo.s_helyseg;
+create table s_orszag as select * from hajo.s_orszag;
+
+update s_helyseg set lakossag = lakossag / 2
+where orszag in (
+    select h.orszag from s_helyseg h
+    join s_orszag o on h.orszag = o.orszag
+    where foldresz = 'Afrika'
+) and lakossag > 500000
+
+--8. feladat
+create view nezet as
+select kikoto_id, helysegnev, orszag, count(*) as utak_szama from hajo.s_kikoto
+join hajo.s_helyseg on helyseg = helyseg_id
+full join hajo.s_ut on kikoto_id = erkezesi_kikoto
+group by kikoto_id, helysegnev, orszag
+
+--9. feladat
+--create view nezet as
+select unique kikoto_id, helysegnev, orszag from hajo.s_kikoto
+join hajo.s_helyseg on helyseg = helyseg_id
+join hajo.s_megrendeles on erkezesi_kikoto = kikoto_id
+where megrendeles_id in (
+    select megrendeles from hajo.s_hozzarendel
+    where rakomanysuly = (
+        select max(rakomanysuly) from hajo.s_hozzarendel
+    )
+)
+
+--10. feladat
+grant delete on s_orszag to panovics;
+revoke delete on s_orszag from panovics

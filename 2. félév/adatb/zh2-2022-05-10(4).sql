42c16dc588dc2fb9de1ce2f8ded96ceae0769926
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-05-11 15:27:28 +0300

    Update zh2-2022-05-10(4).sql

--- 2. félév/adatb/zh2-2022-05-10(4).sql
+++ 2. félév/adatb/zh2-2022-05-10(4).sql
@@ -5,3 +5,25 @@ left join hajo.s_hajo h on hajo_tipus = hajo_tipus_id
 order by tipusnev, hajonev
 
 --2. feladat
+select unique indulasi_kikoto kikotok from hajo.s_ut
+where erkezesi_kikoto = 'It_Cat'
+union
+select unique erkezesi_kikoto from hajo.s_megrendeles
+where indulasi_kikoto = 'It_Cat'
+order by kikotok
+
+--3. feladat
+select vezeteknev, keresztnev, count(*) from hajo.s_ugyfel
+join hajo.s_megrendeles on ugyfel = ugyfel_id
+group by vezeteknev, keresztnev
+order by count(*) desc
+fetch first 4 rows only
+
+--4. feladat
+create table s_kikoto_email (
+    kikoto_id varchar2(10) references s_kikoto(kikoto_id),
+    email varchar2(200),
+    constraint osszk primary key(kikoto_id, email)
+)
+
+--5. feladat

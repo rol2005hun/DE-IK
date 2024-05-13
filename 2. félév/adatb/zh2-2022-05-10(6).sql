cb306bd216ceea7ada9f89ccb1fa88b375f8ca78
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-05-13 15:56:31 +0300

    Update zh2-2022-05-10(6).sql

--- 2. félév/adatb/zh2-2022-05-10(6).sql
+++ 2. félév/adatb/zh2-2022-05-10(6).sql
@@ -44,3 +44,44 @@ alter table s_hajo drop constraint nev
 alter table s_hajo_tipus drop constraint nev
 drop table s_hajo
 drop table s_hajo_tipus
+
+--6. feladat
+drop table s_ugyfel;
+create table s_ugyfel as select * from hajo.s_ugyfel;
+insert into s_ugyfel
+select u.* from hajo.s_ugyfel u
+join hajo.s_helyseg on helyseg = helyseg_id
+where orszag = 'Olaszország'
+
+--7. feladat
+drop table s_helyseg;
+drop table s_orszag;
+create table s_helyseg as select * from hajo.s_helyseg;
+create table s_orszag as select * from hajo.s_orszag;
+
+update s_helyseg set lakossag = round(lakossag / 2)
+where helysegnev in (
+    select helysegnev from hajo.s_helyseg h
+    join hajo.s_orszag o on o.orszag = h.orszag
+    where foldresz = 'Afrika'
+) and lakossag > 500000
+
+--8. feladat
+create view nezet as
+select kikoto_id, helysegnev, orszag, count(erkezesi_kikoto) as megr_sz from hajo.s_kikoto
+left join hajo.s_megrendeles on erkezesi_kikoto = kikoto_id
+join hajo.s_helyseg on helyseg = helyseg_id
+group by kikoto_id, helysegnev, orszag
+
+--9. feladat
+select kikoto_id, helysegnev, orszag from hajo.s_kikoto
+join hajo.s_helyseg on helyseg = helyseg_id
+join hajo.s_megrendeles on indulasi_kikoto = kikoto_id
+join hajo.s_hozzarendel on megrendeles = megrendeles_id
+where rakomanysuly = (
+    select max(rakomanysuly) from hajo.s_hozzarendel
+)
+
+--10. feladat
+grant select on s_ut to panovics;
+revoke select on s_ut from panovics;

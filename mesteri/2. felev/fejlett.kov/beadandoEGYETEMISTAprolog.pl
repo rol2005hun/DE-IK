24d52eb4b488151955bf45a27f8b12f7a1e375e4
Test User <test@example.com>
2026-05-15 21:47:29 +0200

    mentes3

--- mesteri/2. felev/fejlett.kov/beadandoEGYETEMISTAprolog.pl
+++ mesteri/2. felev/fejlett.kov/beadandoEGYETEMISTAprolog.pl
@@ -7,6 +7,42 @@ megoldas(Diakok) :-
         ['vadkerti', _, _, _]
     ],
     
+    % 1. A FENYVESI vezetéknevű LÁNY JOGOT tanul, de NEM DEBRECENben.
+    member(['fenyvesi', FenyvesiKeresztneve, FenyvesiEgyeteme, 'jog'], Diakok),
+    member(FenyvesiKeresztneve, ['edina', 'frida', 'gabriella']),
+    member(FenyvesiEgyeteme, ['budapest', 'miskolc', 'pecs', 'szeged']),
+    
+    % 2. JÓZSEF (ő NEM GALLYAS) az egyik FŐVÁROSI(BUDAPEST) egyetemen tanul, de NEM BIOLÓGIÁt.
+    member([JozsefVezetekneve, 'jozsef', 'budapest', JozsefSzakja], Diakok),
+    JozsefVezetekneve \= 'gallyas',
+    member(JozsefSzakja, ['informatika', 'jog', 'kemia', 'magyar']),
+    
+    % 3. VADKERTI GABRIELLA NEM a SZEGEDI-KÉMIA szakos hallgató.
+    % (A szegedi kémiás létezésének kikényszerítése)
+    member([_, _, 'szeged', 'kemia'], Diakok),
+    member(['vadkerti', 'gabriella', VadkertiEgyeteme, VadkertiSzakja], Diakok),
+    member(VadkertiEgyeteme, ['budapest', 'debrecen', 'miskolc', 'pecs', 'szeged']),
+    member(VadkertiSzakja, ['biologia', 'informatika', 'jog', 'kemia', 'magyar']),
+    \+ (VadkertiEgyeteme = 'szeged', VadkertiSzakja = 'kemia'),
+    
+    % 4. JENEY (PÉCS) keresztneve NEM VINCE.
+    member(['jeney', JeneyKeresztneve, 'pecs', _], Diakok),
+    member(JeneyKeresztneve, ['edina', 'frida', 'gabriella', 'jozsef']),
+    
+    % 5. FRIDA MAGYARtanár szeretne lenni.
+    member([_, 'frida', _, 'magyar'], Diakok),
+    
+    % 6. EDINA vezetékneve VAGY EGRESSY, VAGY MISKOLCon tanul.
+    member([EdinaVezetekneve, 'edina', EdinaEgyeteme, _], Diakok),
+    member(EdinaEgyeteme, ['budapest', 'debrecen', 'miskolc', 'pecs', 'szeged']),
+    (   EdinaVezetekneve = 'egressy', EdinaEgyeteme \= 'miskolc'
+    ;   EdinaVezetekneve \= 'egressy', EdinaEgyeteme = 'miskolc'
+    ),
+    
+    % 7. Az INFORMATIKUSnak készűlő egyetemista nemrég NŐSÜLT. (tehát csak fiú lehet)
+    member([_, InformatikusKeresztneve, _, 'informatika'], Diakok),
+    member(InformatikusKeresztneve, ['jozsef', 'vince']),
+
     member([_, 'edina', _, _], Diakok),
     member([_, 'frida', _, _], Diakok),
     member([_, 'gabriella', _, _], Diakok),
@@ -23,35 +59,4 @@ megoldas(Diakok) :-
     member([_, _, _, 'informatika'], Diakok),
     member([_, _, _, 'jog'], Diakok),
     member([_, _, _, 'kemia'], Diakok),
-    member([_, _, _, 'magyar'], Diakok),
-    
-    % 1. A FENYVESI vezetéknevű LÁNY JOGOT tanul, de NEM DEBRECENben.
-    member(['fenyvesi', FenyvesiKeresztneve, FenyvesiEgyeteme, 'jog'], Diakok),
-    member(FenyvesiKeresztneve, ['edina', 'frida', 'gabriella']),
-    FenyvesiEgyeteme \= 'debrecen',
-    
-    % 2. JÓZSEF (ő NEM GALLYAS) az egyik FŐVÁROSI(BUDAPEST) egyetemen tanul, de NEM BIOLÓGIÁt.
-    member([JozsefVezetekneve, 'jozsef', 'budapest', JozsefSzakja], Diakok),
-    JozsefVezetekneve \= 'gallyas',
-    JozsefSzakja \= 'biologia',
-    
-    % 3. VADKERTI GABRIELLA NEM a SZEGEDI-KÉMIA szakos hallgató.
-    member([_, _, 'szeged', 'kemia'], Diakok),
-    member(['vadkerti', 'gabriella', VadkertiEgyeteme, VadkertiSzakja], Diakok),
-    \+ (VadkertiEgyeteme = 'szeged', VadkertiSzakja = 'kemia'),
-    
-    % 4. JENEY (PÉCS) keresztneve NEM VINCE.
-    member(['jeney', JeneyKeresztneve, 'pecs', _], Diakok),
-    JeneyKeresztneve \= 'vince',
-    
-    % 5. FRIDA MAGYARtanár szeretne lenni.
-    member([_, 'frida', _, 'magyar'], Diakok),
-    
-    % 6. EDINA vezetékneve VAGY EGRESSY, VAGY MISKOLCon tanul.
-    member([EdinaVezetekneve, 'edina', EdinaEgyeteme, _], Diakok),
-    ((EdinaVezetekneve = 'egressy', EdinaEgyeteme \= 'miskolc');
-    (EdinaVezetekneve \= 'egressy', EdinaEgyeteme = 'miskolc')),
-    
-    % 7. Az INFORMATIKUSnak készűlő egyetemista nemrég NŐSÜLT. (tehát csak fiú lehet)
-    member([_, InformatikusKeresztneve, _, 'informatika'], Diakok),
-    member(InformatikusKeresztneve, ['jozsef', 'vince']).
+    member([_, _, _, 'magyar'], Diakok).
